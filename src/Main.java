21fed354f90b370cdc64c7de6b213a145380c989
Drissdo <dothanhdat185@gmail.com>
2024-03-05 21:55:16 +0700

    "init"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new CrosswordPuzzle(10,10);
+        JFrame frame = new JFrame("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
     }
 }
